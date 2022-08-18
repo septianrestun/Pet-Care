2449e16e8b620eb895abd938e8bd73f8f71902df
septianrestun <sseptianrestu@gmail.com>
2022-08-18 11:03:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const menuOpen = document.querySelector(".menu-toggle");
+const menuWrapper = document.querySelector(".menu-wrapper");
+
+menuOpen.addEventListener("click", () => {
+    menuOpen.classList.toggle("bx-x");
+    menuWrapper.classList.toggle("active")
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
